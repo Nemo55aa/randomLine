76e11b309ffe53a6fb9b72500c67fc2ea805171e
Nemo55aa <minato.work1@gmail.com>
2024-03-17 09:08:25 +0900

    テキストエディタとして作り始め

--- desktopWizardTest.cpp
+++ desktopWizardTest.cpp
@@ -17,8 +17,9 @@
 #define OPEN_BUTTON_ID 2
 #define SAVE_BUTTON_ID 3
 #define EDIT_ID 10
+#include <commdlg.h>
 
-void doingFileStuff(LPWSTR str);
+void saveStrToFile(LPWSTR str, LPWSTR filename);
 
 
 #define MAX_LOADSTRING 100
@@ -128,7 +129,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+      CW_USEDEFAULT, 0, 640, 480, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
@@ -163,6 +164,8 @@ LPWSTR strText;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    static OPENFILENAMEW ofn = { 0 };
+    static TCHAR strFile[MAX_PATH], strCustom[256] = TEXT("Before files\0*.*\0\0");
     setlocale(LC_ALL, "ja_JP");
 
     switch (message)
@@ -215,51 +218,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             ((LPCREATESTRUCT)(lParam))->hInstance, NULL
         );
         break;
-    case WM_LBUTTONDOWN:
-        {
-            isPressing = true;
-            CHAR chStr[128];
-            wsprintf((LPWSTR)chStr, L"clicked on: %d, %d", LOWORD(lParam), HIWORD(lParam));
-        }
-    break;
-    case WM_LBUTTONUP:
-        {
-            isPressing = false;
-        }
-    break;
-    case WM_MOUSEMOVE:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = GetDC(hWnd);
-            CHAR chStr[128];
-            wsprintf((LPWSTR)chStr, L"mouse at: %d, %d, \npressing: %d", LOWORD(lParam), HIWORD(lParam), isPressing);
-            TextOut(hdc, 10, 430, (LPWSTR)chStr, sizeof(chStr));
-
-            if (isPressing) {
-                drawDot(hdc, LOWORD(lParam), HIWORD(lParam));
-            }
-            ReleaseDC(hWnd, hdc);
-        }
-    break;
-    case WM_TIMER:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = GetDC(hWnd);
-        CHAR chStr[128];
-        
-        //OnPaint(hdc, GetRandom(10, 1000), GetRandom(10, 1000), GetRandom(10, 1000), GetRandom(10, 1000));
-        
-        wsprintf((LPWSTR)chStr, L"timerCnt: %d", timerCnt++);
-        TextOut(hdc, 10, 270, (LPWSTR)chStr, sizeof(chStr));
-        
-        //(void)wcstombs_s(NULL, (wchar_t*)chStr, (size_t)sizeof(buf), (const char*)buf, 32);
-        (void)mbstowcs_s(NULL, (wchar_t*)chStr, (size_t)sizeof(buf), (const char*)buf, 32);
-
-        TextOut(hdc, 10, 290, (LPWSTR)chStr, sizeof(buf));
-
-        ReleaseDC(hWnd, hdc);
-    }
-    break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -269,12 +227,33 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             case SEND_BUTTON_ID:
                 send(s, "hoge", sizeof("hoge"), 0);
                 break;
+            case OPEN_BUTTON_ID:
+                ofn.lStructSize = sizeof(OPENFILENAME);
+                ofn.hwndOwner = hWnd;
+                ofn.lpstrFilter = TEXT("Text files {*.txt}\0*.txt\0");
+                ofn.lpstrCustomFilter = strCustom;
+                ofn.nMaxCustFilter = 256;
+                ofn.nFilterIndex = 0;
+                ofn.lpstrFile = strFile;
+                ofn.nMaxFile = MAX_PATH;
+                ofn.Flags = OFN_FILEMUSTEXIST;
+                GetOpenFileName((LPOPENFILENAMEW)&ofn);
+                break;
             case SAVE_BUTTON_ID:
+                ofn.lStructSize = sizeof(OPENFILENAME);
+                ofn.hwndOwner = hWnd;
+                ofn.lpstrFilter = TEXT("Text files {*.txt}\0*.txt\0")
+                    TEXT("All files {*.*}\0*.*\0\0");
+                ;
+                ofn.lpstrFile = strFile;
+                ofn.nMaxFile = MAX_PATH;
+                ofn.Flags = OFN_FILEMUSTEXIST | OFN_OVERWRITEPROMPT;
+                GetSaveFileName(&ofn);
+
                 strText = (LPWSTR)malloc(32767);
                 GetWindowText(edit, strText, 32767);
-                doingFileStuff(strText);
+                saveStrToFile(strFile, strText);
                 free(strText);
-
                 break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
@@ -292,7 +271,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: HDC を使用する描画コードをここに追加してください...
-            //OnPaint(hdc, 10,100, 200,300); 
             TextOut(hdc, 10, 200, L"hello", strlen("hello"));
 
             EndPaint(hWnd, &ps);
@@ -330,15 +308,11 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 }
 
 
-void doingFileStuff(LPWSTR str) {
+void saveStrToFile(LPWSTR filename, LPWSTR str) {
     char string[100];
     FILE* sfile;
-    _wfopen_s(&sfile, L"./test.txt", L"a");
-    if (sfile == NULL) {
-        fprintf(stderr, "cannot open %s\n", string);
-        exit(1);
-    }
-    //fprintf(sfile, L"%s\n", str);
+    errno_t er;
+    er = _wfopen_s(&sfile, filename, L"a");
     fwprintf(sfile, L"%s\n", str);
-    fclose(sfile);
+    _fcloseall();
 }
